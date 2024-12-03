d9f77c4085403b39a71e2403df62e3236a5cf687
Lolita <shinkava13@gmail.com>
2024-12-03 15:11:39 +0700

    lint 2

--- test_app.py
+++ test_app.py
@@ -16,8 +16,8 @@ class TestAPI(unittest.TestCase):
         self.assertIn(('test', 2), result['top_words'])
 
     def test_missing_text(self):
-      response = requests.post(self.BASE_URL, json={})
-      self.assertEqual(response.status_code, 400)
+        response = requests.post(self.BASE_URL, json={})
+        self.assertEqual(response.status_code, 400)
 
 
 if __name__ == '__main__':
