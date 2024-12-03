2e830ec4c550b5081f496d8be4c6aaf4ce98d0fd
Lolita <shinkava13@gmail.com>
2024-12-03 15:53:31 +0700

    20

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5000)
     args = parser.parse_args()
-    app.run(port=args.port) 
+    app.run(port=args.port)
