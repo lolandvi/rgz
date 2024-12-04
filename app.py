05058982f9530c1fdf1c365e827b8284b795aca2
Lolita <shinkava13@gmail.com>
2024-12-04 21:35:56 +0700

    done 1

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import argparse
+import logging
 from flask import Flask, request, jsonify, render_template
 from collections import Counter
-import json
 import re
+import socket
+
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 @app.route('/', methods=['GET'])
 def serve_index():
@@ -15,16 +18,20 @@ def serve_index():
 @app.route('/analyze', methods=['POST'])
 def analyze_text():
     try:
-        data = json.loads(request.data)
+        data = request.get_json()
         text = data['text']
         words = re.findall(r'\b\w+\b', text.lower())
         word_counts = Counter(words)
         total_words = len(words)
         top_words = word_counts.most_common(10)
+        server_port = request.environ.get('SERVER_PORT') #получаем порт из окружения
+        server_name = socket.gethostname() #получаем имя хоста
 
         result = {
             'total_words': total_words,
-            'top_words': top_words
+            'top_words': top_words,
+            'server_port': server_port,
+            'server_name': server_name
         }
         return jsonify(result)
     except KeyError:
@@ -38,3 +45,5 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=int, default=5000)
     args = parser.parse_args()
     app.run(port=args.port)
+
+
